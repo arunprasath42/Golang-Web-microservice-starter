9d72dae67e79580827a08fde0ae65e7b1418120a
mhdirfan3 <mi541562@gmail.com>
2022-04-21 12:13:04 +0530

    List pagination and FindByid

--- src/repository/mysqlRepository.go
+++ src/repository/mysqlRepository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"sondr-backend/utils/database"
 )
 
@@ -21,17 +22,17 @@ func (r *MySqlRepositoryRepo) Insert(req interface{}) error {
 }
 
 /***Fetching data from database***/
-func (r *MySqlRepositoryRepo) FindById(obj interface{}, id int) error {
-	if err := database.DB.Debug().Where("unique_id = ? ", id).Find(obj).Error; err != nil {
+func (r *MySqlRepositoryRepo) FindById(obj interface{}, id uint) error {
+	if err := database.DB.Debug().Where("id = ? ", id).Find(obj).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
 /***For Updating tableName ***/
-func (r *MySqlRepositoryRepo) Update(obj interface{}, id int, update interface{}) error {
+func (r *MySqlRepositoryRepo) Update(obj interface{}, whereQuery string, id uint, update interface{}) error {
 
-	if err := database.DB.Debug().Where("Unique_id IN (?) ", id).First(obj).Updates(update).Error; err != nil {
+	if err := database.DB.Debug().Where(whereQuery, id).First(obj).Updates(update).Error; err != nil {
 		return err
 	}
 	return nil
@@ -42,4 +43,36 @@ func (r *MySqlRepositoryRepo) Delete(obj interface{}, id int) error {
 		return err
 	}
 	return nil
+}
+func (r *MySqlRepositoryRepo) ListAllWithPagination(obj interface{}, selectQuery, tableName, joinsQuery string, pageno, pagesize int) (int, error) {
+	var count int
+	var err error
+
+	offset := (pageno - 1) * pagesize
+
+	db := database.DB.Debug().Table(tableName)
+	if selectQuery != "" {
+		db = db.Select(selectQuery)
+	}
+	if joinsQuery != "" {
+		db = db.Joins(joinsQuery)
+	}
+	if err = db.Count(&count).Limit(pagesize).Offset(offset).Find(obj).Error; err != nil {
+		fmt.Println("error in db", err)
+		return count, err
+	}
+	//	log.Println("event", event)
+	return count, nil
+
+}
+
+func (r *MySqlRepositoryRepo) FindByIdWithJoins(obj interface{}, selectQuery, tableName, whereQuery, joinsQuery string, id uint) (int, error) {
+	var count int
+	var err error
+	if err = database.DB.Debug().Table(tableName).Where(whereQuery, id).Select(selectQuery).Joins(joinsQuery).Count(&count).Scan(obj).Error; err != nil {
+		fmt.Println("error in find", err)
+		return count, err
+	}
+	fmt.Println("event", obj)
+	return count, nil
 }
--- src/repository/repository_interface.go
+++ src/repository/repository_interface.go
@@ -5,12 +5,15 @@ import "sondr-backend/src/models"
 /***This interface is common for all the repository files****/
 type MysqlRepository interface {
 	Insert(req interface{}) error
-	FindById(obj interface{}, id int) error
-	Update(obj interface{}, id int, update interface{}) error
+	FindById(obj interface{}, id uint) error
+	Update(obj interface{}, whereQuery string, id uint, update interface{}) error
 	Delete(obj interface{}, id int) error
 	CreateSubadmin(req *models.Admin) error // Admin Repository Interface
+	//ListAllWithPagination()
 
 	/***User Repository Interface***/
 
 	/***Event Repository Interface***/
+	ListAllWithPagination(obj interface{}, selectQuery, tableName, joinsQuery string, pageno, pagesize int) (int, error)
+	FindByIdWithJoins(obj interface{}, selectQuery, tableName, whereQuery, joinsQuery string, id uint) (int, error)
 }
